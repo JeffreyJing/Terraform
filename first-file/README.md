efd4ab018346abbe699afa6b5183559bb5b03352
Jeffrey Jing <jjingdn@gmail.com>
2025-03-12 21:37:18 -0700

    updated first file, can correctly ssh into it now

--- first-file/README.md
+++ first-file/README.md
@@ -7,5 +7,6 @@ create a Terraform file that creates two t2.micro ec2 instances in my AWS accoun
 
 
 Note: 
-Although main.tf successfully creates two ec2 instances in the AWS account, it is now impossible to ssh into either of the two ec2 instances. 
-When spinning up new ec2's manually, AWS will provide a .pem file that allows ssh access into the new instance. Spinning up an ec2 with Terraform skips this step, causing one to be locked out of ssh-ing into this instance. 
+Both the ssh key
+
+I created an instance state object to be able to start and stop the ec2 instances.
--- first-file/main.tf
+++ first-file/main.tf
@@ -1,41 +1,46 @@
 variable "ssh_ip" {}
+
 provider "aws" {
     region = "us-west-1"
 }
 
-resource "aws_instance" "my_first_ec2" {
-    ami           = "ami-038d74967dbfcdee5"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.jeff_ssh.id]
-    tags = {
-        Name = "1st EBS-Terraform-EC2 Demo Machine"
-    }
-}
-
-resource "aws_instance" "my_second_ec2" {
-    ami           = "ami-038d74967dbfcdee5"
-    instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.jeff_ssh.id]
-    tags = {
-        Name = "2nd EBS-Terraform-EC2 Demo Machine"
-    }
+resource "aws_key_pair" "jeff_key" {
+    key_name = "jeff_aws_key"
+    public_key = file("~/.ssh/my_aws_key.pub")
 }
 
 resource "aws_security_group" "jeff_ssh" {
-    name = "jeff_ssh"
-    description = "allow jeff to ssh"
+    name        = "jeff_ssh"
+    description = "Allow Jeff to SSH"
 
     ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["${var.ssh_ip}/32"]
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["${var.ssh_ip}/32"]
     }
 
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1" # "-1" = all protocols
+      cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+resource "aws_instance" "my_ec2" {
+    count                  = 2
+    ami                    = "ami-038d74967dbfcdee5"
+    instance_type          = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.jeff_ssh.id]
+    key_name               = aws_key_pair.jeff_key.key_name
+    tags = {
+        Name = "Terraform-EC2-${count.index + 1}"
+    }
+}
+
+resource "aws_ec2_instance_state" "stop_my_ec2" {
+    for_each    = { for index, instance in aws_instance.my_ec2 : index => instance }
+    instance_id = each.value.id
+    state       = "stopped"
 }
