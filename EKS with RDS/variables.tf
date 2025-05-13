df689212e8fd1da81dc9bf67cfcce99a1319a3e8
Jeffrey Jing <jjingdn@gmail.com>
2025-05-13 16:00:30 -0700

    added user credentials to variables.tf

--- EKS with RDS/variables.tf
+++ EKS with RDS/variables.tf
@@ -37,3 +37,12 @@ variable "public_subnets" {
 variable "private_subnets" {
     type = list(string)
 }
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
